4903a5260885a3af811ed25edbf1e3862e5e5695
Haeryu <hae06177@gmail.com>
2025-03-31 18:55:37 +0900

    deldted unused

--- src/cuda_context.zig
+++ src/cuda_context.zig
@@ -5,18 +5,10 @@ const std = @import("std");
 const Stream = @import("stream.zig").Stream;
 
 pub const CudaContext = struct {
-    cublas_handle: c.cublasHandle_t = null,
     threads_per_block: c_uint = 0,
     curand_generator: c.curandGenerator_t = null,
-    cublaslt_handle: c.cublasLtHandle_t = null,
-    cublaslt_workspace_size: usize = 32 * 1024 * 1024,
-    cublaslt_workspace: ?*anyopaque = null,
 
     pub fn init() !CudaContext {
-        var cublas_handle: c.cublasHandle_t = null;
-        try err.checkCublas(c.cublasCreate(&cublas_handle));
-        errdefer _ = c.cublasDestroy(cublas_handle);
-
         var threads_per_block: c_int = 0;
         try err.checkCuda(c.cudaDeviceGetAttribute(&threads_per_block, c.cudaDevAttrMaxThreadsPerBlock, 0));
         threads_per_block = @min(threads_per_block, 256);
@@ -26,22 +18,9 @@ pub const CudaContext = struct {
         errdefer _ = c.curandDestroyGenerator(curand_generator);
         try err.checkCurand(c.curandSetPseudoRandomGeneratorSeed(curand_generator, @intCast(std.time.timestamp())));
 
-        var cublaslt_handle: c.cublasLtHandle_t = null;
-        try err.checkCublas(c.cublasLtCreate(&cublaslt_handle));
-        errdefer _ = c.cublasLtDestroy(cublaslt_handle);
-
-        const cublaslt_workspace_size = 32 * 1024 * 1024;
-        var cublaslt_workspace: ?*anyopaque = null;
-        try err.checkCuda(c.cudaMalloc(&cublaslt_workspace, cublaslt_workspace_size));
-        errdefer _ = c.cudaFree(cublaslt_workspace);
-
         return .{
-            .cublas_handle = cublas_handle,
             .threads_per_block = @intCast(threads_per_block),
             .curand_generator = curand_generator,
-            .cublaslt_handle = cublaslt_handle,
-            .cublaslt_workspace_size = cublaslt_workspace_size,
-            .cublaslt_workspace = cublaslt_workspace,
         };
     }
 
@@ -50,10 +29,6 @@ pub const CudaContext = struct {
     }
 
     pub fn deinit(self: *CudaContext) void {
-        _ = c.cudaFree(self.cublaslt_workspace);
-        _ = c.cublasLtDestroy(self.cublaslt_handle);
         _ = c.curandDestroyGenerator(self.curand_generator);
-        _ = c.cublasDestroy(self.cublas_handle);
-        self.cublas_handle = null;
     }
 };
